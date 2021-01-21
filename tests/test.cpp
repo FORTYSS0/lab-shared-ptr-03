5770f11854bf4dc7d1e562c931dabc70dd318abb
FORTYSS <dmitriy_sviridov_2001@mail.ru>
2021-01-21 18:13:36 +0300

    finish

--- tests/test.cpp
+++ tests/test.cpp
@@ -12,9 +12,9 @@ TEST(Example, EmptyTest) {
   std::cout<<(sp)<< " " <<sp.use_count() <<std::endl;
   int val = 5;
   SharedPtr<int> sp1 (&val);
- /* SharedPtr<int> sp2(sp1);
+  SharedPtr<int> sp2(sp1);
   SharedPtr<int> sp3(sp2);
-  SharedPtr<int> spoz(new int (10));
+  /*SharedPtr<int> spoz(new int (10));
   std::cout<<sp1.use_count()<<std::endl;
   Values val1;
   val1.val=20;
