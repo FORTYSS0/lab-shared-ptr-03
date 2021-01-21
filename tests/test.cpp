e47caf5d6d8dd251fcc0350a9d9a38516a375943
FORTYSS <dmitriy_sviridov_2001@mail.ru>
2021-01-21 18:09:53 +0300

    finish

--- tests/test.cpp
+++ tests/test.cpp
@@ -10,7 +10,7 @@ TEST(Example, EmptyTest) {
   };
   SharedPtr<int> sp;
   std::cout<<(sp)<< " " <<sp.use_count() <<std::endl;
-  int val = 5;
+ /* int val = 5;
   SharedPtr<int> sp1 (&val);
   SharedPtr<int> sp2(sp1);
   SharedPtr<int> sp3(sp2);
@@ -23,7 +23,7 @@ TEST(Example, EmptyTest) {
   std::cout<<*sp1<<" "<<*sp2<<" "<<*sp3<<" "<<*spoz<<std::endl;
   std::cout<<sp1.get()<<" "<<sp2.get()<<" "<<sp3.get()<<" "<<spoz.get()<<std::endl;
    std::cout<<(sp)<<" "<<(sp1)<<std::endl;
- /* sp1.reset();
+  sp1.reset();
   sp2.reset(new int(10));
   std::cout<<(sp1)<<" "<<*sp2<<std::endl;
   std::cout<<*sp3<<" "<<*spoz<<std::endl;
