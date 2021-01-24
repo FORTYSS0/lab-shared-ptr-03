1973253d70e0ff90253739c2c7d8a3b1a50905e0
FORTYSS <dmitriy_sviridov_2001@mail.ru>
2021-01-24 08:51:15 +0300

    finish

--- tests/test.cpp
+++ tests/test.cpp
@@ -4,33 +4,7 @@
 #include "SharedPtr.hpp"
 
 TEST(Example, EmptyTest) {
-  class Values{
-   public:
-    int val;
-  };
-  SharedPtr<int> sp;
-  std::cout<<(sp)<< " " <<sp.use_count() <<std::endl;
-  int val = 5, val2 = 30;
-  SharedPtr<int> sp1 (&val);
-  SharedPtr<int> sp2(sp1);
-  SharedPtr<int> sp3(sp2);
-  SharedPtr<int> spoz(&val2);
-  std::cout<<sp1.use_count()<<std::endl;
-  Values val1;
-  val1.val=20;
-  SharedPtr<Values> sp5(&val1);
-  std::cout<< sp5->val<<std::endl;
-  std::cout<<*sp1<<" "<<*sp2<<" "<<*sp3<<" "<<*spoz<<std::endl;
-  std::cout<<sp1.get()<<" "<<sp2.get()<<" "<<sp3.get()<<" "<<spoz.get()<<std::endl;
-  std::cout<<(sp)<<" "<<(sp1)<<std::endl;
-  sp1.reset();
-  sp2.reset(&val2);
-  std::cout<<(sp1)<<" "<<*sp2<<std::endl;
-  std::cout<<*sp3<<" "<<*spoz<<std::endl;
-  sp3.swap(spoz);
-  std::cout<<*sp3<<" "<<*spoz<<std::endl;
-  EXPECT_TRUE(true);
-  EXPECT_EQ((sp),0);
+    EXPECT_TRUE(true);
 }
 TEST(Test, Zerro) {
   SharedPtr<int> sp;
